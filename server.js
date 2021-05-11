f82d2fe63283ec9d68f5218c08474d4fb02f25f6
unknown <j0s3.90jr@gmail.com>
2021-05-11 22:58:41 +0200

    Actualizando repositorio

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const express = require('express');
 const app = express();
-app.use(express.static(__dirname + '/mi-primera-furgo-front-v2'));
-app.get('/*', function(req,res){
-res.sendFile(path.join(__dirname, 'mi-primera-furgo-front-v2', 'index.html'))
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/mi-primera-furgo-front-v2'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/mi-primera-furgo-front-v2/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
